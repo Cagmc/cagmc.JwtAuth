932106aa652941196fb709fafdefaa01409c5b20
István Rózsa <istvan.rozsa@outlook.com>
2025-02-12 20:26:59 +0100

    Switch authentication mode to support both Jwt and Cookie
    Updated the authentication flow to dynamically handle Jwt or Cookie-based authentication based on the configuration. Adjusted tests and HttpClient setup to accommodate the selected mode.

--- cagmc.JwtAuth.WebApi.Test/TestBase.cs
+++ cagmc.JwtAuth.WebApi.Test/TestBase.cs
@@ -1,6 +1,9 @@
 ﻿using System.Net.Http.Json;
 using System.Reflection;
+using cagmc.JwtAuth.WebApi.Common.Enum;
 using cagmc.JwtAuth.WebApi.Service;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Xunit.Abstractions;
 
 namespace cagmc.JwtAuth.WebApi.Test;
@@ -8,11 +11,13 @@ namespace cagmc.JwtAuth.WebApi.Test;
 public abstract class TestBase : IClassFixture<WebApiFactory>
 {
     protected WebApiFactory Factory { get; }
+    protected IConfiguration Configuration { get; }
 
     protected TestBase(ITestOutputHelper testOutputHelper, WebApiFactory factory)
     {
         Factory = factory;
         Factory.TestId = GetTestName(testOutputHelper);
+        Configuration = Factory.Services.GetRequiredService<IConfiguration>();
     }
 
     private static string GetTestName(ITestOutputHelper testOutputHelper)
@@ -30,24 +35,33 @@ public abstract class TestBase : IClassFixture<WebApiFactory>
     protected async Task<HttpClient> GetAuthenticatedClientAsync(string username, string password = "<PASSWORD>")
     {
         var client = Factory.CreateClient();
-
+        var authenticationMode = Configuration.GetValue<AuthenticationMode>("AuthenticationMode");
+        
         var loginRequest = new LoginRequest
         {
             Username = username,
-            Password = password
+            Password = password,
+            AuthenticationMode = authenticationMode
         };
         
         var httpResponseMessage = await client.PostAsJsonAsync("/api/accounts/login", loginRequest);
-        
         httpResponseMessage.EnsureSuccessStatusCode();
-    
-        var loginResponse = await httpResponseMessage.Content.ReadFromJsonAsync<LoginResponse>();
-        Assert.NotNull(loginResponse);
+
+        if (authenticationMode == AuthenticationMode.Jwt)
+        {
+            var loginResponse = await httpResponseMessage.Content.ReadFromJsonAsync<LoginResponse>();
+            Assert.NotNull(loginResponse);
+            
+            var token = loginResponse.Token;
+            Assert.False(string.IsNullOrEmpty(token), "Token should not be null or empty.");
         
-        var token = loginResponse.Token;
-        Assert.False(string.IsNullOrEmpty(token), "Token should not be null or empty.");
-    
-        client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+        }
+        else
+        {
+            var cookie = httpResponseMessage.Headers.GetValues("Set-Cookie").FirstOrDefault();
+            client.DefaultRequestHeaders.Add("Cookie", cookie!);
+        }
         
         return client;
     }
